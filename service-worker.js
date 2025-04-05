5b94f3d7ca4f921d82e763a487be70df5eeeaa6c
Erica Viana <96803212+ericaviana12@users.noreply.github.com>
2025-04-05 03:07:48 -0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const cacheName = 'mao-zerada-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/cartas.js',
+  '/baralho.js',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
